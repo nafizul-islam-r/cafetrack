dbd728477b310d783bcb052f634e648ee97f150f
nafizul-islam-r <smnafizulislam@gmail.com>
2025-08-27 01:45:33 +0600

    bootstrap+logos added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
@@ -20,6 +21,9 @@
         @endif
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="bg-primary">
+            <h1>Welcome to Laravel</h1>
+        </div>
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
@@ -172,5 +176,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
     </body>
 </html>
